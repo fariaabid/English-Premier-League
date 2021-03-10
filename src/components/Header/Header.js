5969973ce3520022895805b8c834e41a4de2c36b
faria abid <faria.ju.2k16@gmail.com>
2021-03-10 23:21:32 +0600

    first day final push

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import Teams from '../Teams/Teams';
+import React, { useEffect, useState } from "react";
+import Teams from "../Teams/Teams";
+import "../Header/Header.css";
+
 
 const Header = () => {
-    const [allTeams,setAllTeams] = useState([])
+  const [allTeams, setAllTeams] = useState([]);
 
-    useEffect(() => {
-        const url = "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=English%20Premier%20League";
-        fetch(url)
-        .then(response => response.json())
-        .then(data => setAllTeams(data.teams))
-    })
-    return (
-        <div className="container">
-            <div className="row banner">
-                <h1>English Premier League</h1>
-            </div>
-            <div className="row">
-                {
-                    allTeams.map(team => <Teams team={team}></Teams>)
-                }
-            </div>
+  useEffect(() => {
+    const url =
+      "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=English%20Premier%20League";
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setAllTeams(data.teams));
+  });
+  return (
+    <div>
+      <div className=" banner">
+        <h1 className="text-black text-center bannerTitle">English Premier League</h1>
+      </div>
+      <div style={{backgroundColor:'#00695f'}}>
+      <div className="container">
+        <div className="row">
+          {allTeams.map((team) => (
+            <Teams team={team}></Teams>
+          ))}
         </div>
-    );
+      </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Teams/Teams.js
+++ src/components/Teams/Teams.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight} from '@fortawesome/free-solid-svg-icons'
 
 const Teams = (props) => {
   const { strTeamBadge, strTeam, strSport } = props.team;
   return (
-    <div className="col-md-4">
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={strTeamBadge} />
+        <div className="col-md-4 text-center">
+      <Card style={{ width: "300px",height:'370px',backgroundColor:'white'}} className="my-5">
+        <Card.Img variant="top" style={{ width: "200px",height:'200px'}} className="img-fluid pt-5 rounded mx-auto d-block" src={strTeamBadge} />
         <Card.Body>
-          <Card.Title>{strTeam}</Card.Title>
+          <Card.Title >{strTeam}</Card.Title>
           <Card.Text>{strSport}</Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Button variant="primary"> Explore   <FontAwesomeIcon icon={faArrowRight} /> </Button>
         </Card.Body>
       </Card>
     </div>
